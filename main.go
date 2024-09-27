ac7ac6845901ba94fd8f1bda2369e9d0ea45056e
Markus Wüstenberg <markus@maragu.dk>
2024-09-27 11:23:57 +0200

    Make POC of talking to LLM from CLI

--- main.go
+++ main.go
@@ -1,18 +1,23 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"embed"
+	"fmt"
+	"io"
 	"io/fs"
 	"os"
 	"os/signal"
 	"path/filepath"
+	"strings"
 	"syscall"
 
 	"maragu.dev/errors"
 	"maragu.dev/migrate"
 	"maragu.dev/snorkel"
 
+	"maragu.dev/goat/llm"
 	"maragu.dev/goo/sql"
 )
 
@@ -55,6 +60,45 @@ func start(log *snorkel.Logger) error {
 
 	if err := migrate.Up(ctx, h.DB.DB, subFS); err != nil {
 		return errors.Wrap(err, "error migrating")
+	}
+
+	var messages []llm.Message
+	c := llm.NewOpenAIClient(llm.NewOpenAIClientOptions{
+		BaseURL: "http://localhost:8090/v1",
+		Model:   llm.ModelLlama3_2_1B,
+	})
+
+	fmt.Print("> ")
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		text := scanner.Text()
+
+		fmt.Println()
+
+		messages = append(messages, llm.Message{
+			Content: text,
+			Name:    "Me",
+			Role:    llm.MessageRoleUser,
+		})
+
+		var b strings.Builder
+		w := io.MultiWriter(os.Stdout, &b)
+
+		if err := c.Prompt(ctx, "You are an assistant.", messages, w); err != nil {
+			_, _ = fmt.Fprintln(os.Stderr, "Error: ", err)
+		}
+
+		fmt.Println()
+		fmt.Println()
+
+		messages = append(messages, llm.Message{
+			Content: b.String(),
+			Name:    "Assistant",
+			Role:    llm.MessageRoleAssistant,
+		})
+
+		fmt.Print("> ")
 	}
 
 	return nil
